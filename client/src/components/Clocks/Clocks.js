18de05cecf72a5189c075d85469e9dc72c64cc15
Ghaidan Alhubaishi <alghaidan@yahoo.com>
2019-05-08 17:34:18 -0700

    added clocks.css, got the timer to start and pause

--- client/src/components/Clocks/Clocks.js
+++ client/src/components/Clocks/Clocks.js
@@ -1,30 +1,76 @@
 import React, { Component } from 'react';
+import '../Clocks/clocks.css'
 
 class Clocks extends Component {
     constructor(props) {
         super(props);
-        this.state = { value: '' };
+        this.state = {
+            cardName: '',
+            timerStarted: false,
+            timerStopped: true,
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+            logTime: [],
+        };
 
         this.handleChange = this.handleChange.bind(this);
         // this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
-        this.setState({ value: event.target.value });
+        this.setState({ cardName: event.target.value });
+    }
+
+    handleTimerStart(e) {
+        e.preventDefault();
+        if (this.state.timerStopped) {
+            this.timer = setInterval(() => {
+                this.setState({ timerStarted: true, timerStopped: false });
+                if (this.state.timerStarted) {
+                    if(this.state.seconds >= 60) {
+                        this.setState((prevState) => ({ minutes: prevState.minutes + 1, seconds: 0}))
+                    } 
+                    if(this.state.minutes >= 60){
+                        this.setState((prevState) => ({ hours: prevState.hours + 1, minutes: 0, seconds: 0 }))
+                    }
+                    this.setState((prevState) => ({ seconds: prevState.seconds + 1 }))
+                }
+
+            }, 1000);
+        }
+    }
+
+    handleTimerStop(e){
+        e.preventDefault();
+
+        this.setState({timerStarted: false, timerStopped: true})
+        clearInterval(this.timer)
     }
 
     render() {
         return (
             <div>
-            <div class="card" >
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Ready to clock in?</h6>
-                    <p class="card-text">You have worked (time) so far!</p>
-                    <button href="#" class="card-link">Clock In</button>
-                    <button href="#" class="card-link">View Timesheet</button>
+                <div className="card" >
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <h6 className="card-subtitle mb-2 text-muted">Ready to clock in?</h6>
+                        <p className="card-text">You have worked (time) so far!</p>
+                        <div className="container">
+                            <div className="timer-container">
+                                <div className="current-timer">
+                                    {this.state.hours + ":" + this.state.minutes + ":" + this.state.seconds}
+                                </div>
+                                <div className="timer-controls">
+                                    <button className="btn btn-success" onClick={this.handleTimerStart.bind(this)}>Start</button>
+                                    <button className="btn btn-secondary" onClick={this.handleTimerStop.bind(this)}>Stop</button>
+                                    <button className="btn btn-info">Log Time</button>
+                                    <button className="btn btn-danger">Reset</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
             </div>
         )
     }
